d74b3e84d7f938ca24e484be53da341877c6358c
CauaTr <caua.trevisanuto@gmail.com>
2026-03-04 17:24:07 -0300

    Primeiro Valor

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
+//Import de Utility
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        //Adicionar o scanner no código
+        Scanner input = new Scanner(System.in);
+        //Coleta do primeiro número
+        System.out.println("Primeiro número");
+        float primeiroVal = input.nextFloat();//Fazer em float porque não necessáriamente o usuário vai colocar um número inteiro
+        //Coleta do segundo número
+
     }
 }
