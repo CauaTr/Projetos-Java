2a03180a8d3f2be758451b7f6fd0634f8360b647
CT <caua.trevisanuto@gmail.com>
2026-03-05 09:37:25 -0300

    Char

--- Main.java
+++ Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args) {
         //Adicionar o scanner no código
         Scanner input = new Scanner(System.in);
+
+        //Pede o tipo de operação
+        System.out.println("Qual operação você deseja fazer?\nsoma = '+'\nsubração = '-'\nmuliplicação = '*'\ndivisão = '/'");
+        char operador = input.next().charAt(0);
+        
         //Coleta do primeiro número
         System.out.println("Primeiro número:");
         float primeiroVal = input.nextFloat();//Fazer em float porque não necessáriamente o usuário vai colocar um número inteiro
